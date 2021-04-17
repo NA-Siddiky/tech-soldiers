badfdb9a12aa471593e23c8906efd6ea639e35e5
NA-Siddiky <siddiky.academic@gmail.com>
2021-04-17 22:34:26 +0600

    adding reviews

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import Reviews from '../../Review/Reviews/Reviews';
 import Footer from '../../Shared/Footer/Footer';
 import Clients from '../Clients/Clients';
 import Contact from '../Contact/Contact';
 import Header from '../Header/Header';
-import Reviews from '../Reviews/Reviews';
 import ServiceCollection from '../ServiceCollection/ServiceCollection';
 import SpecialFeatures from '../SpecialFeatures/SpecialFeatures';
 
